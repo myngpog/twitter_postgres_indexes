386b4a5502817e2531471576dca0e094a819279f
My (Mi) Nguyen <71903484+myngpog@users.noreply.github.com>
2024-04-14 13:23:12 -0700

    added both the files from last assignment

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-files='/data/tweets/geoTwitter21-01-01.zip
-/data/tweets/geoTwitter21-01-02.zip
-/data/tweets/geoTwitter21-01-03.zip
-/data/tweets/geoTwitter21-01-04.zip
-/data/tweets/geoTwitter21-01-05.zip
-/data/tweets/geoTwitter21-01-06.zip
-/data/tweets/geoTwitter21-01-07.zip
-/data/tweets/geoTwitter21-01-08.zip
-/data/tweets/geoTwitter21-01-09.zip
-/data/tweets/geoTwitter21-01-10.zip'
-
+files=$(find data/*)
 
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-echo "$files" | time parallel sh load_denormalized.sh
+echo "$files" | time parallel ./load_denormalized.sh
+
+# for file in data/*; do
+    # sh load_denormalized.sh $file
+# done
+
+echo '================================================================================'
+echo 'load pg_normalized'
+echo '================================================================================'
+echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:13457/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs
+echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:13458/ --inputs
